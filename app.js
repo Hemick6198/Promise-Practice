6ce8c740ed253063edd1f4262ef58cbca40037c9
Ethan <ethan.nordahl1@gmail.com>
2023-04-12 00:04:08 -0700

    promise example

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-const emailRef = document.querySelector(".email");
+const statusRef = document.querySelector(".status")
+const videoRef = document.querySelector(".video")
 
-
-fetch("https://jsonplaceholder.typicode.com/users/1")
-    .then((response) => {
-        return response.json();
-})
-    .then((data) => {
-        emailRef.innerHTML = data.email
+function getSubscriptionStatus() {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Log-in");
+        }, 2000);
     })
+}
+
+function getVideo(subscriptionStatus) {
+    return new Promise ((resolve, reject) => {
+        if (subscriptionStatus === "VIP") {
+            resolve("Show video");
+        }
+        else if (subscriptionStatus === "FREE") {
+            resolve("Show trailer");
+        }
+        else {
+            reject("No video");
+        }
+    }) 
+}
+
+async function main() {
+    const status = await getSubscriptionStatus();
+    statusRef.innerHTML = status;
+    try {
+        console.log(await getVideo(status));
+    }
+    catch (e) {
+        console.log(e);
+        videoRef.innerHTML = e;
+    }
+}
+
+main();
